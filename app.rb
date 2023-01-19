1600cf49d1a2fda7e5deeecb74161c545758c90d
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-01-19 23:02:12 +0100

    Menus

--- app.rb
+++ app.rb
@@ -17,6 +17,9 @@ puts "Tape une lettre pour avoir l'information (bien croustillante) qui t'intér
 puts
 puts "a - Le nombre INCROYABLE de handle dans la liste"
 puts "b - Le handle le plus court de notre liste"
+puts "c - Veux-tu découvrir le handle de 5 caractères ? Il est EXTREMEMENT spécial !"
+puts "d - Veux-tu trier tes handle alphabétiquement ?"
+puts "e - Veux-tu trier tes handle par le nombre de caractères ?"
 puts
 
 print "Fais ton choix, maintenant et à jamais : "
@@ -24,6 +27,12 @@ ok = gets.chomp
 
 if ok == "a"
     a = county (awry)
-else
+elsif
     b = short(awry)
+elsif
+    c = fivecarac(awry)
+elsif
+    d = alpha(awry)
+else
+    d = longueur(awry)
 end
